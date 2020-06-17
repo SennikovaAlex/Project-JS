1d51c524e351cc510148cb769da5dee5476f38d4
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-06-17 21:55:10 +0300

    lesson09 leght

---
+++ index.js
@@ -0,0 +1,30 @@
+const calcButton = document.getElementById('start');
+const plusButton1 = document.getElementsByTagName('button')[0];
+const plusButton2 = document.getElementsByTagName('button')[1];
+const checkbox = document.querySelector('#deposit-check');
+const input = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const depositAmount = document.querySelector('.deposit-amount');
+const depositPercent = document.querySelector('.deposit-percent');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+
+
+
+
+
+
+
+
